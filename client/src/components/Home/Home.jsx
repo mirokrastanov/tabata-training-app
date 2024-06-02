bf5f7133f3a268602e568004f6c33c10bc007cb2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-02 22:38:37 +0300

    Update structure and initial tailwind classes

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import './Home.css';
 import { FaPlay, FaEllipsisVertical, FaArrowLeft, FaGear, FaPlus } from "react-icons/fa6";
 
 
 function Home() {
 
     return (
-        <div id="app__wrapper">
+        <div id="app__wrapper" className="bg-purple-900 text-white min-w-full max-w-[23rem] max-h-[46rem] flex flex-nowrap flex-col justify-start items-stretch rounded-lg">
             {/* Change with views */}
-            <div id="app__titlebar">
+            <div id="app__titlebar" className="p-3">
                 <div><FaArrowLeft /></div>
                 <h3>Workouts: 2</h3>
                 <div><FaGear /></div>
             </div>
 
-            <div id="workouts__wrapper">
-                <article className="workout__card">
+            <div id="workouts__wrapper" className="bg-white p-1.5 flex flex-col gap-1.5">
+                <article className="workout__card rounded p-3">
                     <section className="card__top">
                         <h3>HIIT Circuit 1</h3>
                         <div className="card__top-btn"><FaPlay /></div>
