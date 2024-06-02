0229fb4c4afc491b5f85eb5c3972c80aefefb9e1
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-03 00:51:55 +0300

    Update design and add render workout cards dynamically

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Home.css';
-import { FaPlay, FaEllipsisVertical, FaArrowLeft, FaGear, FaPlus } from "react-icons/fa6";
+import { FaPlay, FaEllipsisVertical, FaArrowLeft, FaGear, FaPlus, FaArrowsUpDown } from "react-icons/fa6";
+import { bgColors } from '../../utils/colors';
+import WorkoutCard from '../workoutCard/WorkoutCard';
 
 
 function Home() {
 
     return (
-        <div id="app__wrapper" className="bg-purple-900 text-white min-w-full max-w-[23rem] max-h-[46rem] flex flex-nowrap flex-col justify-start items-stretch rounded-lg">
+        <div id="app__wrapper" className="bg-purple-900 text-white min-w-full max-w-[23rem] max-h-[46rem] flex flex-nowrap flex-col justify-start items-stretch rounded-lg relative">
             {/* Change with views */}
             <div id="app__titlebar" className="flex flex-row flex-nowrap items-center gap-x-1 h-14 text-xl">
                 <div className="w-14 h-full flex justify-center items-center"><FaArrowLeft /></div>
@@ -16,7 +18,7 @@ function Home() {
                 <div className="w-14 h-full flex justify-center items-center"><FaGear /></div>
             </div>
 
-            <div id="workouts__wrapper" className="bg-white p-1.5 flex flex-col gap-1.5">
+            <div id="workouts__wrapper" className="bg-white p-1.5 flex flex-col gap-1.5 h-[calc(100%-3.5rem)] overflow-y-scroll hide-scrollbar">
                 <article className="workout__card rounded p-3">
                     <section className="card__top">
                         <h3>HIIT Circuit 1</h3>
@@ -54,9 +56,15 @@ function Home() {
                         <p>Total: 08:35 - 26 intervals</p>
                     </section>
                 </article>
+
+                {/* When DB is added - map from DB and figure out how to add color
+                so contrast between sequential items is kept */}
+                {bgColors.map((x, i) => (
+                    <WorkoutCard color={x} i={i} key={'card-' + i} />
+                ))}
             </div>
 
-            <div className="app__bottom-overlay">
+            <div className="app__bottom-overlay absolute bottom-0 z-10 bg-black/50">
                 <div className="overlay-info">
                     <p>Tap triangle to start</p>
                     <p>Three dots to edit or delete</p>
@@ -66,6 +74,7 @@ function Home() {
                     <p><FaPlus /></p>
                 </div>
             </div>
+
         </div>
     )
 }
