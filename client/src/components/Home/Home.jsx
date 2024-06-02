bfc5f41e3f8db8e76614190d20dea87553761bba
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-02 21:49:53 +0300

    Complete view 1 - workouts screen

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { FaPlay, FaEllipsisVertical } from "react-icons/fa6";
+import { FaPlay, FaEllipsisVertical, FaArrowLeft, FaGear, FaPlus } from "react-icons/fa6";
 
 
 function Home() {
 
     return (
         <div id="app__wrapper">
-
+            {/* Change with views */}
             <div id="app__titlebar">
-                {/* Change with views */}
-
+                <div><FaArrowLeft /></div>
+                <h3>Workouts: 2</h3>
+                <div><FaGear /></div>
             </div>
 
             <div id="workouts__wrapper">
@@ -32,8 +33,36 @@ function Home() {
                     </section>
                 </article>
 
+                <article className="workout__card">
+                    <section className="card__top">
+                        <h3>HIIT Circuit 2</h3>
+                        <div className="card__top-btn"><FaPlay /></div>
+                        <div className="card__top-btn"><FaEllipsisVertical /></div>
+                    </section>
+                    <section className="card__mid">
+                        <p>Prepare: 30 sec</p>
+                        <p>Work: 45 sec - Jumping Jacks</p>
+                        <p>Rest: 15 sec</p>
+                        <p>Work: 45 sec - Burpees</p>
+                        <p>Rest: 15 sec</p>
+                        <p>...</p>
+                    </section>
+                    <section className="card__bottom">
+                        <p>Total: 08:35 - 26 intervals</p>
+                    </section>
+                </article>
             </div>
 
+            <div className="app__bottom-overlay">
+                <div className="overlay-info">
+                    <p>Tap triangle to start</p>
+                    <p>Three dots to edit or delete</p>
+                    <p>Tap and hold to reorder</p>
+                </div>
+                <div className="overlay-btn">
+                    <p><FaPlus /></p>
+                </div>
+            </div>
         </div>
     )
 }
