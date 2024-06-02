6fd2805b1437ddf13604abe4278442b51724b541
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-02 21:40:17 +0300

    Initial app structure

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { FaPlay, FaEllipsisVertical } from "react-icons/fa6";
+
 
 function Home() {
 
     return (
-        <div>Home</div>
+        <div id="app__wrapper">
+
+            <div id="app__titlebar">
+                {/* Change with views */}
+
+            </div>
+
+            <div id="workouts__wrapper">
+                <article className="workout__card">
+                    <section className="card__top">
+                        <h3>HIIT Circuit 1</h3>
+                        <div className="card__top-btn"><FaPlay /></div>
+                        <div className="card__top-btn"><FaEllipsisVertical /></div>
+                    </section>
+                    <section className="card__mid">
+                        <p>Prepare: 30 sec</p>
+                        <p>Work: 45 sec - Jumping Jacks</p>
+                        <p>Rest: 15 sec</p>
+                        <p>Work: 45 sec - Burpees</p>
+                        <p>Rest: 15 sec</p>
+                        <p>...</p>
+                    </section>
+                    <section className="card__bottom">
+                        <p>Total: 08:35 - 26 intervals</p>
+                    </section>
+                </article>
+
+            </div>
+
+        </div>
     )
 }
 
