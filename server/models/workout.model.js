129609ba12ffb5cebacedab2719837817a459f88
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-07 12:02:45 +0300

    Create a workout model

---
+++ server/models/workout.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const workoutSchema = new mongoose.Schema({
+    creatorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, 'Creator ID required'],
+    },
+    preparation: { type: Number, required: true },
+    break: { type: Number, required: true },
+    cooldown: { type: Number, required: true },
+    exercises: [
+        {
+            exercise: {
+                type: String,
+                required: true,
+            },
+            duration: {
+                type: Number,
+                required: true,
+            },
+            orderIndex: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    // createdAt, updatedAt
+}, { timestamps: true });
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+export default Workout;
