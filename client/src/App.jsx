ea0136b91a97736414e08c15f56ed6a76f307060
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-10 13:18:04 +0300

    Move home fragments to App.jsx and adjust

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,47 @@
 import { useState } from 'react';
 import './App.css';
 import { Toaster } from 'react-hot-toast';
+import { FaPlay, FaEllipsisVertical, FaArrowLeft, FaGear, FaPlus, FaArrowsUpDown, FaArrowRightFromBracket } from "react-icons/fa6";
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
+import Workouts from './components/workouts/Workouts';
 
 function App() {
 
     return (
         <>
             <Toaster />
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/home" element={<Navigate to="/" />} />
-                <Route path="/index" element={<Navigate to="/" />} />
 
-            </Routes>
+            <div id="app__wrapper" className="bg-purple-900 text-white min-w-full max-w-[23rem] max-h-[46rem] flex flex-nowrap flex-col justify-start items-stretch rounded-xl relative">
+
+                <div id="app__titlebar" className="w-full flex flex-row flex-nowrap items-center gap-x-1 h-14 text-xl">
+                    {/* TODO --> Change content inside based on page requirements */}
+                    <div className="w-14 h-full flex justify-center items-center hover:bg-purple-600 rounded-xl rounded-bl-none transition-all cursor-pointer hover:shadow-md active:scale-90 active:rounded-bl-xl">
+                        
+                        {/* Conditional rendering based on param passed */}
+                        <FaArrowRightFromBracket className="rotate-180" />
+                        {/* <FaArrowLeft /> */}
+                    </div>
+                    <div className="titlebar-title h-full flex justify-center items-center px-2 w-[calc(100%-7.5rem)]">
+                        <h2 className="text-ellipsis line-clamp-1 font-bold">Workouts: 2</h2>
+                    </div>
+                    <div className="w-14 h-full flex justify-center items-center hover:bg-purple-600 rounded-xl rounded-br-none transition-all cursor-pointer hover:shadow-md active:scale-90 active:rounded-br-xl"><FaGear /></div>
+                </div>
+
+                <Routes>
+                    <Route exact path="/" element={<Workouts />} />
+                    <Route path="/home" element={<Navigate to="/" />} />
+                    <Route path="/index" element={<Navigate to="/" />} />
+                    {/* ADD More routes and finish with a wildcard route */}
+                </Routes>
+
+                {/* TODO --> Render conditionally only on certain pages */}
+                <div className="app__bottom-overlay w-full absolute bottom-0 z-10 bg-black/50">
+                    <div className="overlay-btn w-full">
+                        <div className="bg-purple-900 hover:bg-purple-600 text-white w-14 h-14 absolute bottom-5 right-5 flex justify-center items-center text-xl cursor-pointer transition-all rounded-full hover:rounded-2xl active:scale-90"><FaPlus /></div>
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
