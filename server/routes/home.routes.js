5d425eec6695deb2c73e2e62b41d65b6a6ed1d08
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-13 11:10:09 +0300

    Create a dev test route for session testing

--- server/routes/home.routes.js
+++ server/routes/home.routes.js
@@ -7,6 +7,27 @@ router.get('/', (req, res) => {
     res.send('home');
 });
 
+
+router.get('/test', (req, res) => {
+    const resultObject = {};
+    console.log(req.session);
+    resultObject.session = req.session;
+    console.log(req.session.id);
+    resultObject.id = req.session.id;
+    req.session.visited = true; // to keep the initial session id for subsequent requests
+    req.sessionStore.get(req.session.id, (err, sessionData) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        console.log(sessionData);
+        resultObject.sessionData = sessionData;
+    });
+    res.status(200).send({ msg: "Test endpoint", ...resultObject });
+});
+
+
+
 router.get('/404', (req, res) => {
     res.send('404');
 });
