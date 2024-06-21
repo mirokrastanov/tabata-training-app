d2b01daac3a42477765e3b279af04e2e062975ac
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-21 22:33:58 +0300

    Update user route guard for non-jwt auth

--- server/src/middlewares/userRoute.middleware.js
+++ server/src/middlewares/userRoute.middleware.js
@@ -4,27 +4,21 @@ import User from '../models/user.model.js';
 // TODO - update after implementing sessions and cookies fully
 const userRoute = async (req, res, next) => {
     try {
-        const token = req.cookies.jwt;
+        // const token = req.cookies.jwt;
+        // if (!token) {
+        //     return res.status(401).json({ error: 'Unauthorized - No token provided' });
+        // }
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // if (!decoded) {
+        //     return res.status(401).json({ error: 'Unauthorized - Invalid token' });
+        // }
+        // const user = await User.findById(decoded.userId).select('-password');
+        // if (!user) {
+        //     return res.status(401).json({ error: 'User not found' });
+        // }
 
-        if (!token) {
-            return res.status(401).json({ error: 'Unauthorized - No token provided' });
-        }
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        if (!decoded) {
-            return res.status(401).json({ error: 'Unauthorized - Invalid token' });
-        }
-
-        const user = await User.findById(decoded.userId).select('-password');
-
-        if (!user) {
-            return res.status(401).json({ error: 'User not found' });
-        }
-
-        req.user = user;
-        next();
-
+        if (req.user) return next();
+        res.status(401).json({ error: 'Unauthorized. You need to login.' });
     } catch (error) {
         console.log('Error in userRoute middleware: ', error.message);
         res.status(500).json({ error: 'Internal Server Error' });
