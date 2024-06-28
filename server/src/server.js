59f340c4d2e3870aa3b3fd0fe5a5a6c170f33d3c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-28 18:21:25 +0300

    Add an extra passport strategy and an error handler

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,8 @@ import routes from './routes.js';
 import connectToMongoDB from './config/mongoDB.js';
 import sessionConfig from './config/sessionConfig.js';
 import passport from 'passport';
-import './config/passporConfig.js';
+import { localStrategy, discordStrategy } from './config/passporConfig.js';
+import { preRoutesErrorHandler } from './middlewares/errorHandlers.middleware.js';
 
 dotenv.config();
 const app = express();
@@ -22,8 +23,13 @@ app.use(cookieParser());
 app.use(sessionConfig);
 
 // Passport.js
+passport.use(localStrategy);
+passport.use(discordStrategy);
 app.use(passport.initialize()); // adds req.login, req.logout, etc
 app.use(passport.session());
+
+// Pre-routes error handler
+app.use(preRoutesErrorHandler);
 
 // Routes
 app.use(routes);
