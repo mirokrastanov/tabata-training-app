c32f89450ae98fc1a110cc39981f5122a14d8394
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-28 18:17:05 +0300

    Update the users only middleware

--- server/src/middlewares/userRoute.middleware.js
+++ server/src/middlewares/userRoute.middleware.js
@@ -1,12 +1,21 @@
-
-
 const userRoute = async (req, res, next) => {
     try {
         if (req.user) return next();
-        res.status(401).json({ error: 'Unauthorized. You need to login.', user: req.user });
+        res.status(401).json({
+            error: 'Unauthorized. You need to login.',
+            user: req.user,
+            session: req.session,
+            sessionID: req.sessionID
+        });
     } catch (error) {
         console.log('Error in userRoute middleware: ', error.message);
-        res.status(500).json({ error: 'Internal Server Error', msg: error.message, user: req.user });
+        res.status(500).json({
+            error: 'Internal Server Error',
+            msg: error.message,
+            user: req.user,
+            session: req.session,
+            sessionID: req.sessionID
+        });
     }
 };
 
