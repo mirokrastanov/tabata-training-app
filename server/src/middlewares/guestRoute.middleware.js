4026bba5982ade58885f4a068bb636e3329e8cd4
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-28 18:16:53 +0300

    Update the guest only middleware

--- server/src/middlewares/guestRoute.middleware.js
+++ server/src/middlewares/guestRoute.middleware.js
@@ -1,15 +1,21 @@
-// TODO - update after implementing sessions and cookies fully
-
 const guestRoute = async (req, res, next) => {
     try {
-        const token = req.cookies.jwt;
-        if (token) {
-            return res.status(401).json({ error: 'You are already athenticated' });
-        }
-        next();
+        if (!req.user) return next();
+        res.status(401).json({
+            error: 'You are already logged in.',
+            user: req.user,
+            session: req.session,
+            sessionID: req.sessionID
+        });
     } catch (error) {
         console.log('Error in guestRoute middleware: ', error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({
+            error: 'Internal Server Error',
+            msg: error.message,
+            user: req.user,
+            session: req.session,
+            sessionID: req.sessionID
+        });
     }
 };
 
