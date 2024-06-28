97d7496f37f52e99a5365232e907dc012ecd58e8
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-28 18:15:39 +0300

    Add discord passport config

--- server/src/config/passporConfig.js
+++ server/src/config/passporConfig.js
@@ -1,26 +1,22 @@
-import { Strategy } from 'passport-local';
-import User from "../models/user.model.js";
-import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
 import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as DiscordStrategy } from 'passport-discord';
+import bcrypt from 'bcryptjs';
+import User from "../models/user.model.js";
+import DiscordUser from '../models/discordUser.model.js';
+import genError from '../utils/genError.js';
+dotenv.config();
 
-const authenticateUser = async (username, password, done) => {
+const localOptions = { usernameField: 'username', passwordField: 'password' };
+const localStrategy = new LocalStrategy(localOptions, async (username, password, done) => {
     try {
         const user = await User.findOne({ username }).lean();
-        if (!user) throw new Error('User not found');
+        if (!user) throw genError('User not found', { source: 'LocalStrategy', user });
+
         const passwordCheck = await bcrypt.compare(password, user?.password || '');
-        if (!user || !passwordCheck) throw new Error('Invalid credentials');
-        delete user.password;
-        done(null, user);
-    } catch (error) {
-        done(error, null);
-    }
-};
-const localStrategy = new Strategy({ usernameField: 'username', passwordField: 'password' }, authenticateUser);
-passport.serializeUser((user, done) => done(null, user._id));
-passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id).lean();
-        if (!user) throw new Error('User not found');
+        if (!user || !passwordCheck) throw genError('Invalid credentials', { source: 'LocalStrategy', user });
+
         delete user.password;
         done(null, user);
     } catch (error) {
@@ -28,4 +24,42 @@ passport.deserializeUser(async (id, done) => {
     }
 });
 
-export default passport.use(localStrategy);
+const discordOptions = {
+    clientID: process.env.DISCORD_CLIENT_ID,
+    clientSecret: process.env.DISCORD_SECRET,
+    callbackURL: process.env.DISCORD_REDIRECT_URI,
+    scope: ['identify', 'email', 'guilds'], // what to pull from discord - each requires permission
+};
+const discordStrategy = new DiscordStrategy(discordOptions, async (accessToken, refreshToken, profile, done) => {
+    let user;
+    try {
+        user = await DiscordUser.findOne({ discordId: profile.id });
+        if (!user) {
+            const newUser = new DiscordUser({ discordId: profile.id, username: profile.username, profile });
+            await newUser.save();
+
+            user = await DiscordUser.findOne({ discordId: profile.id }).lean();
+        }
+        done(null, user);
+    } catch (error) {
+        done(error, null);
+    }
+});
+
+passport.serializeUser((user, done) => done(null, user._id));
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id).lean() || await DiscordUser.findById(id).lean();
+        if (!user) throw genError('User not found', { source: 'deserializeUser', user });
+
+        delete user.password;
+        done(null, user);
+    } catch (error) {
+        done(error, null);
+    }
+});
+
+export {
+    localStrategy,
+    discordStrategy,
+};
