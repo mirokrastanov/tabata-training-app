fdfd59dc7c383f8420f9b68a26d0d815820cf250
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-06-30 23:17:28 +0300

    Complete getUser testing

--- server/src/__tests__/auth.routes.test.js
+++ server/src/__tests__/auth.routes.test.js
@@ -25,5 +25,36 @@ describe('getUser by ID', () => {
         expect(res.json).toHaveBeenCalledWith(user);
     });
 
-    
+    it('should return 404 if user not found', async () => {
+        User.findById.mockResolvedValue(null);
+
+        await getUser(req, res);
+
+        expect(User.findById).toHaveBeenCalledWith('userId');
+        expect(res.status).toHaveBeenCalledWith(404);
+        expect(res.json).toHaveBeenCalledWith({ msg: 'User not found', user: null });
+    });
+
+    it('should return 400 if invalid user ID is provided', async () => {
+        const error = new Error('CastError');
+        error.name = 'CastError';
+        User.findById.mockRejectedValue(error);
+
+        await getUser(req, res);
+
+        expect(User.findById).toHaveBeenCalledWith('userId');
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ msg: 'User does not exist or the provided ID is invalid' });
+    });
+
+    it('should return 500 for any other errors', async () => {
+        const error = new Error('Internal Server Error');
+        User.findById.mockRejectedValue(error);
+
+        await getUser(req, res);
+
+        expect(User.findById).toHaveBeenCalledWith('userId');
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error', msg: error.message });
+    });
 });
