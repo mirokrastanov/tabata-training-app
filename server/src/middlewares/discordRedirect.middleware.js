7ea620cc4e74810235fbbba0fb751f2a53b9e90b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-07-19 01:13:28 +0300

    Modify discord redirect to handle auth denial errors

--- server/src/middlewares/discordRedirect.middleware.js
+++ server/src/middlewares/discordRedirect.middleware.js
@@ -1,6 +1,28 @@
 const discordRedirect = (req, res, next) => {
+
+    if (!req.query.code) {
+        const queryParams = req.query;
+        console.log("Discord authorization denied! \n", queryParams);
+
+        req.authMethod = undefined;
+        req.user = null;
+
+        return res.status(401).json({
+            msg: "Discord authorization denied!",
+            user: null,
+            errors: { ...queryParams },
+        });
+    }
+
     req.authMethod = 'discord';
     next();
 };
 
 export default discordRedirect;
+
+`
+If Authorization is denied:
+
+http://localhost:8000/api/auth/discord/redirect?error=access_denied&error_description=The+resource+owner+or+authorization+server+denied+the+request
+
+`
