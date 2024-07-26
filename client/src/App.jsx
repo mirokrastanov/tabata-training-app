406d4c0e72fd532764b2b082bb0981477aedba8e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-07-26 21:03:34 +0300

    Update styles and animations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
                 {/* TODO --> Render conditionally only on certain pages */}
                 <div className="app__bottom-overlay w-full absolute bottom-0 z-10 bg-black/50">
                     <div className="overlay-btn w-full">
-                        <div className="bg-purple-900 hover:bg-purple-600 text-white w-20 h-20 absolute bottom-12 right-12 flex justify-center items-center text-3xl cursor-pointer transition-all rounded-full hover:rounded-2xl active:scale-90"><FaPlus /></div>
+                        <div className="bg-purple-800 hover:bg-purple-600 text-white w-16 h-16 absolute bottom-8 right-8 flex justify-center items-center text-2xl cursor-pointer transition-all active:scale-90"><FaPlus /></div>
                     </div>
                 </div>
 
