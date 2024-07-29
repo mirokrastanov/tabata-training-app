a55a931aa3505b790ba770853dd1d58ab2b0933e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-07-29 23:04:54 +0300

    Add custom tooltips to titlebar

--- client/src/components/titlebar/Titlebar.jsx
+++ client/src/components/titlebar/Titlebar.jsx
@@ -5,7 +5,7 @@ import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 
 function Titlebar() {
     const location = useLocation();
-    
+
     // TODO: Different states for different paths
     const path = location.pathname;
 
@@ -20,16 +20,28 @@ function Titlebar() {
     return (
         <div id="app__titlebar" className="w-full flex flex-row flex-nowrap items-center gap-x-1 h-14 text-xl">
             {/* TODO --> Change content inside based on page requirements */}
+
             <div className="w-14 h-full flex justify-center items-center hover:bg-purple-600 rounded-xl rounded-bl-none transition-all cursor-pointer hover:shadow-md active:scale-90 active:rounded-bl-xl">
 
                 {/* Conditional rendering based on param passed */}
-                <FaArrowRightFromBracket className="rotate-180" />
-                {/* <FaArrowLeft /> */}
+                <div className="tooltip-anchor h-full w-14 flex justify-center items-center transition-all">
+                    <FaArrowRightFromBracket className="rotate-180" />
+                    <span className="tooltip tooltip-right">Logout</span>
+                </div>
+
             </div>
+
             <div className="titlebar-title h-full flex justify-center items-center px-2 w-[calc(100%-7.5rem)]">
                 <h2 className="text-ellipsis line-clamp-1 font-bold">Workouts: 2</h2>
             </div>
-            <div className="w-14 h-full flex justify-center items-center hover:bg-purple-600 rounded-xl rounded-br-none transition-all cursor-pointer hover:shadow-md active:scale-90 active:rounded-br-xl"><FaGear /></div>
+
+            <div className="w-14 h-full flex justify-center items-center hover:bg-purple-600 rounded-xl rounded-br-none transition-all cursor-pointer hover:shadow-md active:scale-90 active:rounded-br-xl">
+                <div className="tooltip-anchor h-full w-14 flex justify-center items-center transition-all">
+                    <FaGear />
+                    <span className="tooltip tooltip-left settings-tooltip">Settings</span>
+                </div>
+            </div>
+
         </div>
     )
 }
