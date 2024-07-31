ec72082e561650e93d03b7e77b233dc30bd973d2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-07-31 23:17:25 +0300

    Add create btn custom tooltip

--- client/src/components/shared/addBtn/AddBtn.jsx
+++ client/src/components/shared/addBtn/AddBtn.jsx
@@ -18,7 +18,10 @@ function AddBtn() {
     return (
         <div className="app__bottom-overlay w-full absolute bottom-0 z-10 bg-black/50">
             <div className="overlay-btn w-full">
-                <div className="bg-purple-800 hover:bg-purple-600 text-white w-16 h-16 absolute bottom-8 right-8 flex justify-center items-center text-2xl cursor-pointer transition-all active:scale-90"><FaPlus /></div>
+                <div id="o-tip-anchor" className="bg-purple-800 hover:bg-purple-600 text-white w-16 h-16 absolute bottom-8 right-8 flex justify-center items-center text-2xl cursor-pointer transition-all active:scale-90">
+                    <FaPlus />
+                    <div id="o-tip">Create</div>
+                </div>
             </div>
         </div>
     )
