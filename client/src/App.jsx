fbac12c39bcd40dbf384317cd513c70c942e099a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-04 16:44:35 +0300

    Remove obsolete code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { Toaster } from 'react-hot-toast';
 import { FaPlay, FaEllipsisVertical, FaArrowLeft, FaGear, FaPlus, FaArrowsUpDown, FaArrowRightFromBracket } from "react-icons/fa6";
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Workouts from './components/workouts/Workouts';
-import Login from './components/login/Login';
 import SignUp from './components/signup/SignUp';
 import Titlebar from './components/titlebar/Titlebar';
 import AddBtn from './components/shared/addBtn/AddBtn';
@@ -24,7 +23,6 @@ function App() {
                     <Route exact path="/" element={<Workouts />} />
                     <Route path="home" element={<Navigate to="/" />} />
                     <Route path="index" element={<Navigate to="/" />} />
-                    <Route path="user/login" element={<Login />} />
                     <Route path="user/signup" element={<SignUp />} />
                     <Route path="user/signin" element={<SignIn />} />
                     
