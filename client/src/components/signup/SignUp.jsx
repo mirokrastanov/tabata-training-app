538704e5b5e2ee109af3475de089482bd7ad1db2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-04 16:22:56 +0300

    Update form and submit btn

--- client/src/components/signup/SignUp.jsx
+++ client/src/components/signup/SignUp.jsx
@@ -24,9 +24,9 @@ const SignUp = () => {
 
     return (
         <div id="reg-form-ctr" className="w-full h-full flex items-center justify-center bg-slate-300 rounded-b-xl">
-            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+            <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
                 <h2 className="text-2xl font-bold mb-6 text-gray-800">Sign Up</h2>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} autoComplete="off">
 
                     <FormInput name={'fullName'} v={form.fullName} handler={handleChange} />
                     <FormInput name={'username'} v={form.username} handler={handleChange} />
@@ -34,7 +34,7 @@ const SignUp = () => {
                     <FormInput name={'password'} v={form.password} handler={handleChange} />
                     <FormInput name={'confirmPassword'} v={form.confirmPassword} handler={handleChange} />
 
-                    <button type="submit" className="w-full bg-purple-900 text-white my-6 py-2 rounded-lg hover:bg-purple-600 active:scale-90 transition-all">Sign Up</button>
+                    <button type="submit" className="w-full bg-purple-900 text-white my-6 py-2 rounded-lg  hover:bg-purple-600 focus:outline-none border-none active:scale-90 transition-all">Sign Up</button>
                 </form>
             </div>
         </div>
