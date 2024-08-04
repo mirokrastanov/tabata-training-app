477af91d7178cf85bb0d1cdf3db39d2877fe5c1c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-04 16:08:57 +0300

    Apply custom input to all inputs. Update responsiveness.

--- client/src/components/signup/SignUp.jsx
+++ client/src/components/signup/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './SignUp.css';
+import FormInput from '../shared/formInput/FormInput';
 
 
 const SignUp = () => {
@@ -22,66 +23,18 @@ const SignUp = () => {
     };
 
     return (
-        <div className="w-full h-full flex items-center justify-center bg-slate-300">
+        <div id="reg-form-ctr" className="w-full h-full flex items-center justify-center bg-slate-300 rounded-b-xl">
             <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-6 text-gray-800">Signup</h2>
+                <h2 className="text-2xl font-bold mb-6 text-gray-800">Sign Up</h2>
                 <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Full Name</label>
-                        <input
-                            type="text"
-                            name="fullName"
-                            value={form.fullName}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-600"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Username</label>
-                        <input
-                            type="text"
-                            name="username"
-                            value={form.username}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Email</label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={form.email}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            value={form.password}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Confirm Password</label>
-                        <input
-                            type="password"
-                            name="confirmPassword"
-                            value={form.confirmPassword}
-                            onChange={handleChange}
-                            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
-                    >
-                        Signup
-                    </button>
+
+                    <FormInput name={'fullName'} v={form.fullName} handler={handleChange} />
+                    <FormInput name={'username'} v={form.username} handler={handleChange} />
+                    <FormInput name={'email'} v={form.email} handler={handleChange} />
+                    <FormInput name={'password'} v={form.password} handler={handleChange} />
+                    <FormInput name={'confirmPassword'} v={form.confirmPassword} handler={handleChange} />
+
+                    <button type="submit" className="w-full bg-purple-900 text-white my-6 py-2 rounded-lg hover:bg-purple-600 active:scale-90 transition-all">Sign Up</button>
                 </form>
             </div>
         </div>
