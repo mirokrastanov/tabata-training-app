1c3d6ae26e6c4d8b66100b8bae6de380d3cdfe3d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-04 16:55:13 +0300

    Notes for further development

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,11 @@ function App() {
 
             <div id="app__wrapper" className="bg-purple-900 text-white w-full h-[calc(100vh-4rem)] flex flex-nowrap flex-col justify-start items-stretch rounded-xl relative">
                 <Titlebar />
+                {/* TODO (in title bar) */}
+                {/* ==> Add overlay menu on settings click */}
+                {/* ==> Create CONTEXT for current view. Update the below based on that: */}
+                {/* ==> Update title based on view. Accept param for home view. */}
+                {/* ==> Update signin/logout btn based on view */}
 
                 <Routes>
                     <Route exact path="/" element={<Workouts />} />
@@ -25,7 +30,15 @@ function App() {
                     <Route path="index" element={<Navigate to="/" />} />
                     <Route path="user/signup" element={<SignUp />} />
                     <Route path="user/signin" element={<SignIn />} />
-                    
+                    {/* TODO */}
+                    {/* Create Workout */}
+                    {/* Edit Workout */}
+                    {/* View Workout */}
+
+                    {/* View User Profile */}
+
+                    {/* Workout in progress | Action View | Timers | Main */}
+
 
 
                     <Route path="*" element={<NotFound />} />
