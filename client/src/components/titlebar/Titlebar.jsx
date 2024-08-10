e830e83d4a90974bf5010132d49101ebfe4e996e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-11 01:18:49 +0300

    Render multiple titlebar elements dynamically

--- client/src/components/titlebar/Titlebar.jsx
+++ client/src/components/titlebar/Titlebar.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import './Titlebar.css';
-import { FaPlay, FaEllipsisVertical, FaArrowLeft, FaGear, FaPlus, FaArrowsUpDown, FaArrowRightFromBracket } from "react-icons/fa6";
+import { FaPlay, FaEllipsisVertical, FaArrowLeft, FaGear, FaPlus, FaArrowsUpDown, FaArrowRightFromBracket, FaHouse } from "react-icons/fa6";
 import { Link, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import { usePage } from '../../contexts/PageContext';
 
 function Titlebar() {
-    const { location, pageParams: p } = usePage();
+    const { location: { pathname: path }, pageParams: p } = usePage();
 
     // TODO: Different states for different paths
 
@@ -13,7 +13,7 @@ function Titlebar() {
 
 
     useEffect(() => {
-        console.log(location.pathname);
+        console.log('Path: ', path);
         console.log(p);
 
 
@@ -35,15 +35,20 @@ function Titlebar() {
 
             <div className="titlebar-title h-full flex justify-center items-center px-2 w-[calc(100%-7.5rem)]">
                 <h2 className="text-ellipsis line-clamp-1 font-bold">
-                    {p.title}{p?.params && `: ${p.params.count}`}
+                    {p.title}{path == '/' && `: ${p.params.count}`}
                 </h2>
             </div>
 
             <Link className="w-14 h-full flex justify-center items-center hover:bg-purple-600 rounded-xl rounded-br-none transition-all cursor-pointer hover:shadow-md active:scale-90 active:rounded-br-xl text-white hover:text-white"
-                to="/settings" >
+                to={p.rightBtn.url} >
                 <div className="tooltip-anchor h-full w-14 flex justify-center items-center transition-all">
-                    <FaGear />
-                    <span className="tooltip tooltip-left settings-tooltip">Settings</span>
+                    {(path == '/' ? <FaGear /> : (
+                        path == '/settings' ? <FaHouse /> :
+                            <FaHouse />
+                    ))}
+                    <span className="tooltip tooltip-left settings-tooltip">
+                        {path == p.path ? p.rightBtn.text : 'Home'}
+                    </span>
                 </div>
             </Link>
 
