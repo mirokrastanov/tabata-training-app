8ea46a71f3eaea6e4fe7c6094a135d80b5040cb7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-11 01:18:27 +0300

    Update page param templates

--- client/src/utils/pageParamLibrary.js
+++ client/src/utils/pageParamLibrary.js
@@ -1,27 +1,26 @@
 const defaultCase = {
     path: '*',
-    title: 'Tabata Training',
-    leftBtn: '',
-    rightBtn: '',
-    params: {},
+    title: 'Not Found',
+    leftBtn: { icon: '', url: '' }, // update based on guest / user state
+    rightBtn: { icon: 'FaHome', url: '/', text: 'Home' },
+    params: {}, // update based on DB fetched data (if any)
 };
 
 const workouts = {
     path: '/',
     title: 'Workouts',
-    leftBtn: '',
-    rightBtn: '',
-    params: {
-        count: 24,
-    },
+    leftBtn: { icon: '', url: '' }, // update based on guest / user state
+    rightBtn: { icon: 'FaGear', url: '/settings', text: 'Settings' },
+    params: { count: 24 }, // update based on DB fetched data (if any)
 };
 const settings = {
     path: '/settings',
     title: 'Settings',
-    leftBtn: '',
-    rightBtn: '',
-    params: {},
+    leftBtn: { icon: '', url: '' }, // update based on guest / user state
+    rightBtn: { icon: 'FaHouse', url: '/', text: 'Home' },
+    params: {}, // update based on DB fetched data (if any)
 };
+
 
 export {
     defaultCase,
