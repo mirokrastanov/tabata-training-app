a97131e4818a3450f148a2973ca9afa29ee31561
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-10 21:08:40 +0300

    Create page library util

---
+++ client/src/utils/pageParams.js
@@ -0,0 +1,19 @@
+export const getPageParams = (path) => {
+    const workouts = {
+        path: '/',
+        title: 'Workouts',
+        leftBtn: '',
+        rightBtn: '',
+        titleParams: {
+            workoutCount: 24,
+        },
+    };
+    const settings = {
+        path: '/settings',
+        title: 'Settings',
+        leftBtn: '',
+        rightBtn: '',
+        titleParams: {},
+    };
+
+};
