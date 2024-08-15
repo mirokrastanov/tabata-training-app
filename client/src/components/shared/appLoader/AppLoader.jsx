fe084dad749b14fa733a442a272495f71cefb751
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-15 23:07:55 +0300

    Update app loader. Make it responsive.

--- client/src/components/shared/appLoader/AppLoader.jsx
+++ client/src/components/shared/appLoader/AppLoader.jsx
@@ -3,7 +3,9 @@ import './AppLoader.css';
 
 function AppLoader() {
     return (
-        <div className="app-loader"></div>
+        <div id="app-loader-ctr" className="flex justify-center items-center h-[calc(100vh-4rem)] w-full">
+            <div className="app-loader"></div>
+        </div>
     )
 }
 
