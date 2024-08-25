2ddc2a241290e2084f7271237e1c99494193b2bf
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-25 18:24:20 +0300

    Remove unnecessary margins

--- client/src/components/btns/HBtnSeparator.jsx
+++ client/src/components/btns/HBtnSeparator.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function HBtnSeparator() {
     return (
-        <div className="flex w-full justify-center items-center">
+        <div className="flex w-full justify-center items-center my-2">
             <hr className="border-0 ml-4 border-t-4 flex-1 z-[1]" />
             <p className="text-black text-lg px-2">or</p>
             <hr className="border-0 mr-4 border-t-4 flex-1 z-[1]" />
