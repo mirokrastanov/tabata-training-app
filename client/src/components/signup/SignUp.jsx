8ab4456a8a2ade354322cc1ea44736aea892d5d7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-25 18:29:40 +0300

    Major form rework and abstraction, incorporating each of the new custom components. Initial preparation for custom form hook solution too.

--- client/src/components/signup/SignUp.jsx
+++ client/src/components/signup/SignUp.jsx
@@ -2,18 +2,14 @@ import React, { useState } from 'react';
 import './SignUp.css';
 import FormInput from '../shared/formInput/FormInput';
 import { Link } from 'react-router-dom';
-import { FaDiscord } from 'react-icons/fa6';
-import { MdAlternateEmail } from "react-icons/md";
-import BtnLoader from '../loaders/final/btnLoader/btnLoader';
 import { useAuth } from '../../contexts/AuthContext';
 import toast from 'react-hot-toast';
-import InactiveBtn from '../btns/inactiveBtn';
 import ActiveBtn from '../btns/activeBtn';
 import HBtnSeparator from '../btns/HBtnSeparator';
 import FormChange from '../btns/FormChange';
+import VBtnSeparator from '../btns/VBtnSeparator';
 
 const SignUp = () => {
-    const [loading, setLoading] = useState(false);
     const [form, setForm] = useState({
         fullName: '',
         username: '',
@@ -31,8 +27,8 @@ const SignUp = () => {
     }
     const chooseDiscordSignUp = (e) => {
         e.preventDefault();
-        // setSignUpType('discord');
-        toast.error(`Hey, line 1 \n line 2 \n line 3...`);
+        setSignUpType('discord');
+        // toast.error(`Hey, line 1 \n line 2 \n line 3...`);
     }
 
     const handleChange = (e) => {
@@ -52,56 +48,46 @@ const SignUp = () => {
         // TODO: manually dismiss the toast in case of issues
     };
 
+    const handleDiscordLogin = async (e) => {
+        e.preventDefault();
+        console.log('Discord login attempted...');
+
+        // await discordLogin();
+    }
+
     return (
         <div id="reg-form-ctr" className="w-full h-[calc(100%-3.5rem)] flex items-center justify-center bg-gray-100 rounded-b-xl">
             <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
                 <h2 className="text-2xl font-bold mb-6 text-gray-800">Sign Up</h2>
-                {signUpType === null
-                    ? (<>
-                        <ActiveBtn handler={chooseLocalSignUp} text={'Use Email'} />
-                        <HBtnSeparator />
-                        <ActiveBtn handler={chooseDiscordSignUp} text={'Use Discord'} />
+                
+                {/* LANDING VIEW */}
+                {signUpType == null && (<>
+                    <ActiveBtn handler={chooseLocalSignUp} text={'Use Email'} />
+                    <HBtnSeparator />
+                    <ActiveBtn handler={chooseDiscordSignUp} text={'Use Discord'} />
+                    <FormChange goTo={'signIn'} />
+                </>)}
+                
+                {/* EMAIL SIGNUP VIEW */}
+                {signUpType === 'local' && (<>
+                    <form onSubmit={handleSubmit}>
+                        <FormInput name={'fullName'} v={form.fullName} handler={handleChange} />
+                        <FormInput name={'username'} v={form.username} handler={handleChange} />
+                        <FormInput name={'email'} v={form.email} handler={handleChange} />
+                        <FormInput name={'password'} v={form.password} handler={handleChange} />
+                        <FormInput name={'confirmPassword'} v={form.confirmPassword} handler={handleChange} />
 
-                        <InactiveBtn /> {/* FOR Submitting state */}
+                        <VBtnSeparator check={false} rIcon={'discord'} rHandler={chooseDiscordSignUp} />
+                    </form>
+                    <FormChange goTo={'signIn'} />
+                </>)}
 
-                        <FormChange goTo={'signIn'} />
-                    </>)
-                    : (<>{signUpType === 'local'
-                        ? (<>
-                            <form onSubmit={handleSubmit} autoComplete="off">
-
-                                <FormInput name={'fullName'} v={form.fullName} handler={handleChange} />
-                                <FormInput name={'username'} v={form.username} handler={handleChange} />
-                                <FormInput name={'email'} v={form.email} handler={handleChange} />
-                                <FormInput name={'password'} v={form.password} handler={handleChange} />
-                                <FormInput name={'confirmPassword'} v={form.confirmPassword} handler={handleChange} />
-
-                                <div className="flex flex-row flex-nowrap justify-between my-6 bg-red-400">
-                                    <button type="submit" className="w-[87%] bg-purple-900 text-white py-2 rounded-lg  hover:bg-purple-600 active:bg-purple-500 focus:outline-none border-none transition-all">
-                                        {loading ? <BtnLoader /> : 'Sign Up'}
-                                    </button>
-                                    <p className="py-2 text-xl text-gray-800">|</p>
-                                    <button type="button" className="text-2xl flex justify-center items-center bg-purple-900 text-white p-2 w-11 h-11 rounded-lg hover:bg-purple-600 active:bg-purple-500 focus:outline-none border-none transition-all" onClick={chooseDiscordSignUp}>
-                                        <FaDiscord />
-                                    </button>
-                                    {/* <InactiveBtn square={true} /> */}
-                                </div>
-                            </form>
-                            <FormChange goTo={'signIn'} />
-                        </>)
-                        : (<>
-                            <div className="flex flex-row flex-nowrap justify-between">
-                                <button className="w-[87%] text-lg flex justify-center items-center gap-2 bg-purple-900 text-white my-6 py-2 rounded-lg hover:bg-purple-600 active:bg-purple-500 focus:outline-none border-none transition-all">
-                                    <FaDiscord className="text-3xl" />
-                                    <p className="max-custom-mq-300:hidden">Use Discord</p>
-                                </button>
-                                <p className="my-6 text-xl py-2 text-gray-800">|</p>
-                                <button type="button" className="text-2xl flex justify-center items-center bg-purple-900 text-white my-6 p-2 rounded-lg hover:bg-purple-600 active:bg-purple-500 focus:outline-none border-none transition-all" onClick={chooseLocalSignUp}><MdAlternateEmail /></button>
-                            </div>
-                            <p className="text-black px-2">Have an Account? <Link to="/user/signin"
-                                className="a-left px-1 pb-1 font-bold hover:text-purple-600" >Sign In</Link></p>
-                        </>)}
-                    </>)}
+                {/* DISCORD SIGNUP VIEW */}
+                {signUpType === 'discord' && (<>
+                    <VBtnSeparator lHandler={handleDiscordLogin} lIcon={true} rHandler={chooseLocalSignUp} />
+                    <FormChange goTo={'signIn'} />
+                </>)}
+                
             </div>
         </div>
     );
