5f9b8489bce41e24e6558694d13f8fdfe2042d9c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-29 00:17:10 +0300

    Create a ring loader base

---
+++ client/src/components/loaders/base/ring/Ring.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Ring.css';
+
+function Ring() {
+    return (
+        <div className="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Ring;
