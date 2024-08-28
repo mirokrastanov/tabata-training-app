10ec2de774976779e57312fd96de0052398b4128
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-28 17:37:32 +0300

    Add user landing view

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import toast from 'react-hot-toast';
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 
 const Home = () => {
     const { user } = useAuth();
@@ -23,9 +24,69 @@ const Home = () => {
             {user
                 ? (
                     <article className="flex-grow bg-slate-400">
-                        <h1>Logged in landing view {'{}'}</h1>
-                        <h2>NYI...</h2>
-                        {/* TODO: Implement a user landing page view */}
+                        <section className="bg-white py-20">
+                            <div className="container mx-auto px-6 text-center">
+                                <h2 className="text-4xl font-bold text-gray-800">Welcome back,
+                                    <Link to={'/user/profile'} className="a-left text-purple-500 font-bold hover:text-purple-700 transition-all hover:mx-0 hover:px-2">
+                                        {user?.username}</Link>!</h2>
+                                <p className="text-gray-600 mt-4">
+                                    Continue where you left off or start a new workout session.
+                                </p>
+                                <div className="flex justify-center mt-8">
+                                    <Link to="/workouts" className="mx-2 px-8 py-4 bg-purple-900 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-purple-600">
+                                        Start New Workout
+                                    </Link>
+                                    {/* TODO: store and render from local storage - create component */}
+                                    <Link to="/workout/history" className="mx-2 px-8 py-4 bg-purple-900 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-purple-600">
+                                        View Progress
+                                    </Link>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section id="recent-activity" className="bg-gray-100 py-20">
+                            <div className="container mx-auto px-6">
+                                <h2 className="text-3xl font-bold text-gray-800">Your Activity</h2>
+                                <div className="mt-10 flex flex-wrap justify-center">
+                                    <div className="w-full sm:w-1/2 lg:w-1/3 px-4 py-6">
+                                        <div className="bg-white p-6 rounded-lg shadow-lg">
+                                            <h3 className="text-2xl font-semibold text-gray-800">Last Workout</h3>
+                                            <p className="text-gray-600 mt-2">Completed on [Date].</p>
+                                        </div>
+                                    </div>
+                                    <div className="w-full sm:w-1/2 lg:w-1/3 px-4 py-6">
+                                        <div className="bg-white p-6 rounded-lg shadow-lg">
+                                            <h3 className="text-2xl font-semibold text-gray-800">Sets Completed</h3>
+                                            <p className="text-gray-600 mt-2">You've completed [amount] sets!</p>
+                                        </div>
+                                    </div>
+                                    <div className="w-full sm:w-1/2 lg:w-1/3 px-4 py-6">
+                                        <div className="bg-white p-6 rounded-lg shadow-lg">
+                                            <h3 className="text-2xl font-semibold text-gray-800">Workout time</h3>
+                                            <p className="text-gray-600 mt-2">You've spent [amount] hours working out.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex justify-center mt-8">
+                                <Link to="/workouts/statistics" className="mx-2 px-8 py-4 bg-purple-900 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-purple-600">
+                                    Full Statistics
+                                </Link>
+                            </div>
+                        </section>
+
+                        <section className="bg-white py-20">
+                            <div className="container mx-auto px-6 text-center">
+                                <h2 className="text-4xl font-bold text-gray-800">Stay Motivated</h2>
+                                <p className="text-gray-600 mt-4">
+                                    Keep pushing your limits and achieve your fitness goals.
+                                </p>
+                                <Link target='_blank' to="https://www.reddit.com/r/Fitness/" className="mt-8 px-8 py-4 bg-purple-900 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-purple-600 flex flex-nowrap justify-center items-center w-fit mx-auto">
+                                    Join the Community 
+                                    <FaArrowUpRightFromSquare className="ml-2" />
+                                </Link>
+                            </div>
+                        </section>
                     </article>
                 )
                 : (
