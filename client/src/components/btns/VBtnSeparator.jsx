9f11b6561646502df7bc9283c928e44fa653ff77
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-08-28 20:36:25 +0300

    Update vertical separator icons and text

--- client/src/components/btns/VBtnSeparator.jsx
+++ client/src/components/btns/VBtnSeparator.jsx
@@ -8,19 +8,18 @@ function VBtnSeparator({ text, lHandler, rHandler, check = false, rIcon, lIcon }
         const { location } = usePage();
 
         switch (location.pathname) {
-            case '/user/signup': text = 'Sign Up'; break;
-            case '/user/signin': text = 'Sign In'; break;
+            case '/user/signup': text = 'Sign Up'; lIcon ? rIcon = 'signup' : null; break;
+            case '/user/signin': text = 'Sign In'; lIcon ? rIcon = 'signin' : null; break;
             default: text = 'Settings'; break;
         }
     }
-    if (lIcon) rIcon = 'email';
 
     return (
         <div className="flex flex-row flex-nowrap justify-between my-6">
             {lIcon ? (<ActiveBtn left87={true} icon={true} handler={lHandler} text={'Use Discord'} />) : <>
                 {check
                     ? (<InactiveBtn left87={true} />)
-                    : (<ActiveBtn icon={false} text={text} left87={true} btnType={'submit'}/>)
+                    : (<ActiveBtn icon={false} text={text} left87={true} btnType={'submit'} />)
                 }
             </>}
             <p className="py-2 text-xl text-gray-800">|</p>
