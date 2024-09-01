117d3fc738e6927e667a931df6b52f091d754326
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-01 16:51:33 +0300

    Update rendering conditions

--- client/src/components/btns/VBtnSeparator.jsx
+++ client/src/components/btns/VBtnSeparator.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import InactiveBtn from './inactiveBtn';
-import ActiveBtn from './activeBtn';
+import ActiveBtn from './ActiveBtn';
 import { usePage } from '../../contexts/PageContext';
 
 function VBtnSeparator({ text, lHandler, rHandler, check = false, rIcon, lIcon }) {
@@ -16,12 +16,11 @@ function VBtnSeparator({ text, lHandler, rHandler, check = false, rIcon, lIcon }
 
     return (
         <div className="flex flex-row flex-nowrap justify-between my-6">
-            {lIcon ? (<ActiveBtn left87={true} icon={true} handler={lHandler} text={'Use Discord'} />) : <>
-                {check
-                    ? (<InactiveBtn left87={true} />)
-                    : (<ActiveBtn icon={false} text={text} left87={true} btnType={'submit'} />)
-                }
-            </>}
+            {check ? (<InactiveBtn left87={true} />) : (<>
+                {lIcon
+                    ? (<ActiveBtn left87={true} icon={true} handler={lHandler} text={'Use Discord'} />)
+                    : (<ActiveBtn icon={false} text={text} left87={true} btnType={'submit'} />)}
+            </>)}
             <p className="py-2 text-xl text-gray-800">|</p>
             <ActiveBtn square={true} handler={rHandler} iconOnly={rIcon} />
         </div>
