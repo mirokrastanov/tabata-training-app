5c5882ffd1e0d6f94934fff3dbd60ebd193d001b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-01 18:30:04 +0300

    Update route guards post auth context change.

--- client/src/routeGuards/GuestRoutes.jsx
+++ client/src/routeGuards/GuestRoutes.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext.jsx';
+import toast from 'react-hot-toast';
 
 export default function GuestRoutes() {
     const { user } = useAuth();
+
+    useEffect(() => {
+        // console.log('GUARD >> GuestRoutes >> User Object:\n', user);
+
+        if (user) toast.error('Re-routing...\nYou are already signed in.');
+    }, []);
+
     return !user
         ? (<Outlet />)
         : (<Navigate to='/' />);
--- client/src/routeGuards/UserRoutes.jsx
+++ client/src/routeGuards/UserRoutes.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext.jsx';
+import toast from 'react-hot-toast';
 
 export default function UserRoutes() {
     const { user } = useAuth();
+
+    useEffect(() => {
+        // console.log('GUARD >> UserRoutes >> User Object:\n', user);
+
+        if (!user) toast.error('Re-routing...\nYou need to sign in to access this page.');
+    }, []);
+
     return user
         ? (<Outlet />)
         : (<Navigate to='/user/signup' />);
