95ecbf0eb39d0332bffd399b877b65ad5a5064f5
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-09 15:09:59 +0300

    Update math function to work with string inputs

--- client/src/utils/math.js
+++ client/src/utils/math.js
@@ -1,9 +1,7 @@
 export const incrementBy1 = (v) => {
-    if (v >= 120) return v;
-    return v + 1;
+    return Number(v) + 1 >= 120 ? v : String(Number(v) + 1);
 };
 
 export const decrementBy1 = (v) => {
-    if (v <= 0) return v;
-    return v - 1;
+    return Number(v) - 1 < 0 ? '0' : String(Number(v) - 1);
 };
