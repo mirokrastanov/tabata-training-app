b001282e46451998464b0d9f29050643c5a92e3b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-10 18:12:39 +0300

    Update page structure. Import context data from workout context. Change title with dynamic input and updates.

--- client/src/pages/CreateWorkout/CreateWorkout.jsx
+++ client/src/pages/CreateWorkout/CreateWorkout.jsx
@@ -15,111 +15,41 @@ import WorkoutHelp from '../../components/workout/workoutInterval/WorkoutHelp';
 import WorkoutInterval from '../../components/workout/workoutInterval/WorkoutInterval';
 import ServiceInterval from '../../components/workout/workoutInterval/ServiceInterval';
 import IntervalSkeleton from '../../components/workout/workoutInterval/IntervalSkeleton';
+import { useWorkout } from '../../contexts/WorkoutContext';
+import { FaPencil } from 'react-icons/fa6';
 
 function CreateWorkout() {
-    const [workoutIntervals, setWorkoutIntervals] = useState(
-        { nextAvailableID: 0, intervals: {} }
-    );
-
-    const sampleWorkout = {
-        creatorId: 'creatorId',
-        workoutName: 'HIIT 2.0',
-        preparation: 30,
-        break: 15,
-        cooldown: 60,
-        exercises: [
-            { exercise: 'Jumping Jacks', duration: 45, orderIndex: 1 },
-        ],
-    };
-    // PREP and COOLDOWN cannot be deleted - make it TRUE!!!
-    // Add a break after each interval
-    // Make sure the breaks cannot be deleted either
-
-    // assign them the same ORDER ID as their previous interval so 
-    // ORDER IDs are stored in the DB object - THUS DO NOT UPDATE ANY single exercise object
-    // and save it on the DB, BEFORE the whole workout has been saved
-    // applicable for both create and edit workout
-
-    // figure out whether view workout should be different and make sure it's locked for editing
-    // and just displays the workout WITH EDIT and PLAY buttons at the bottom and top of the view
-
-
-    // that when it's deleted the corresponding break is deleted too
-    // maybe also attach them as dataset order ids 
-
-    // also because of the sampleWorkout structure
-    // add after each interval a break code that does not change (maybe with a param for WorkoutInterval)
-    // do a separate one with no params for PREP and COOLDOWN with their icons, and without na input
-    // figure out a way to keep the cooldown LAST without constantly updating order ids (unless I figure out a way
-    // to not keep them in a state)
-
-
-
-    const { register, handleSubmit, formState: { errors }, reset }
-        = useForm({ resolver: zodResolver(signUpSchema) });
-
     const [isSubmitting, setIsSubmitting] = useState(false);
-    const { user } = useAuth();
     const navigate = useNavigate();
-
-
-    const sampleWorkoutInterval = {
-        // id, 
-    };
-    const createWorkoutInterval = (e) => {
-        e.preventDefault();
-        setWorkoutIntervals(prev => ({
-            ...prev,
-            intervals: { ...prev.intervals, [workoutIntervals.nextAvailableID]: ['item 1', 'item 2', { d: workoutIntervals.nextAvailableID }] }
-        }))
-        setWorkoutIntervals(prev => ({ ...prev, nextAvailableID: Number(prev.nextAvailableID) + 1 }));
-    };
+    const { user } = useAuth();
+    const {
+        workoutName, cooldown, prep, rest,
+        setWorkoutName, setCooldown, setPrep, setRest,
+        intervals,
+    } = useWorkout();
 
     useEffect(() => {
-        console.log(workoutIntervals);
-    }, [workoutIntervals]);
-
-    const onSubmit = async (validatedData) => {
-        setIsSubmitting(true);
-        // console.log('Form submitted:', validatedData);
-
-        const delayedResponse = async (validatedData) => {
-            // sim delay for testing
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            const response = await registerUser(validatedData);
-            if (!response.ok) throw response;
-            return response;
-        };
-        await toast.promise(delayedResponse(validatedData), {
-            loading: 'Loading...',
-            success: (response) => {
-                setIsSubmitting(false);
-                // reset();
-                return response.msg || 'Request successful!';
-                // return navigate('/?referrer=signup');
-            },
-            error: (error) => {
-                setIsSubmitting(false);
-                return error.msg || error.error || error.message || 'Request failed';
-            },
-        });
-        setTimeout(() => toast.dismiss(), 5000); // Manually dismisses the toast in case of issues
-        navigate('/');
-    };
-
+        console.log(intervals);
+    }, [intervals]);
 
     return (<div id="create-workout-ctr" className="w-full h-[calc(100%-3.5rem)] flex justify-center bg-gray-100 rounded-b-xl overflow-y-scroll py-10">
         <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md h-fit">
-            <h2 className="text-2xl font-bold mb-6 text-gray-800">Current Workout Title</h2>
+            <div className="relative flex justify-center items-center">
+                <input className="text-3xl font-bold text-gray-800 bg-white w-full h-12 text-center"
+                    value={workoutName} onChange={(e) => setWorkoutName(e.target.value)} placeholder="Add a title" />
+                <FaPencil className="text-purple-900 absolute right-0 w-12 h-full p-3 pointer-events-none" />
+            </div>
 
             <WorkoutHelp />
 
             <article id="workout-intervals">
+                <ServiceInterval type='preparation' value={prep} />
+
+
                 {/* MAP the intervals and display them with their corresponding interval type */}
 
                 {/* DURING MAPPING - auto-generate a rest period after each work period  */}
 
-                <ServiceInterval type='preparation' />
                 <WorkoutInterval type='work' />
                 <ServiceInterval type='rest' />
                 <ServiceInterval type='cooldown' />
