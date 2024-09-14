e418356ec41e0f0bd1fb683b78523aae4ca19d90
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-14 13:26:13 +0300

    Add extra workout presets

--- client/src/contexts/WorkoutContext.jsx
+++ client/src/contexts/WorkoutContext.jsx
@@ -100,17 +100,56 @@ export function WorkoutProvider({ children }) {
     // MOVE to util
     const workoutPresets = {
         initial: {
-            workoutName: 'HIIT Workout',
+            workoutName: 'Standard HIIT',
+            prep: '30',
+            rest: '15',
+            cooldown: '60',
+            intervals: [
+                { orderIndex: '1', duration: '30', exercise: 'Jumping Jacks' },
+                { orderIndex: '2', duration: '30', exercise: 'Push-ups' },
+                { orderIndex: '3', duration: '30', exercise: 'Situps' },
+                { orderIndex: '4', duration: '30', exercise: 'Squats' },
+                { orderIndex: '5', duration: '30', exercise: 'Leg Raises' },
+                { orderIndex: '6', duration: '30', exercise: 'Lunges' },
+                { orderIndex: '7', duration: '30', exercise: 'High Knees' },
+                { orderIndex: '8', duration: '30', exercise: 'Russian Twists' },
+            ],
+        },
+        intermediate: {
+            workoutName: 'Intermediate HIIT',
+            prep: '30',
+            rest: '10',
+            cooldown: '60',
+            intervals: [
+                { orderIndex: '1', duration: '30', exercise: 'Jumping Jacks' },
+                { orderIndex: '2', duration: '30', exercise: 'Static Squat' },
+                { orderIndex: '3', duration: '30', exercise: 'Push-ups' },
+                { orderIndex: '4', duration: '30', exercise: 'Crunches' },
+                { orderIndex: '5', duration: '30', exercise: 'Step onto a chair' },
+                { orderIndex: '6', duration: '30', exercise: 'Squats' },
+                { orderIndex: '7', duration: '30', exercise: 'Chair dips' },
+                { orderIndex: '8', duration: '30', exercise: 'Plank' },
+                { orderIndex: '9', duration: '30', exercise: 'High knees' },
+                { orderIndex: '10', duration: '30', exercise: 'Lunges' },
+                { orderIndex: '11', duration: '30', exercise: 'Push-up & rotation' },
+                { orderIndex: '12', duration: '30', exercise: 'Left-side plank' },
+                { orderIndex: '13', duration: '30', exercise: 'Right-side plank' },
+            ]
+        },
+        advanced: {
+            workoutName: 'Advanced HIIT',
             prep: '30',
             rest: '15',
             cooldown: '60',
             intervals: [
                 { orderIndex: '1', duration: '45', exercise: 'Jumping Jacks' },
                 { orderIndex: '2', duration: '45', exercise: 'Burpees' },
-                { orderIndex: '3', duration: '45', exercise: 'Static Squat' },
+                { orderIndex: '7', duration: '45', exercise: 'High Knees' },
+                { orderIndex: '8', duration: '45', exercise: 'Mountain Climbers' },
+                { orderIndex: '9', duration: '45', exercise: 'Jump Squats' },
+                { orderIndex: '9', duration: '45', exercise: 'Alternating Lunges' },
             ],
         },
-        // to find and add 2-3 basic presets
     };
 
     // ONLY USE inside Create Workout Page
