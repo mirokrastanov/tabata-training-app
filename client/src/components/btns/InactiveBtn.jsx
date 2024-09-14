20554092ccc0dc659a457324e958ac10de168f0e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-14 18:40:10 +0300

    Add an additional mode

--- client/src/components/btns/InactiveBtn.jsx
+++ client/src/components/btns/InactiveBtn.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import BtnLoader from '../loaders/final/btnLoader/btnLoader';
 
-function InactiveBtn({ loader = 'wave', square = false, left87 = false }) {
+function InactiveBtn({ loader = 'wave', square = false, left87 = false, left74 = false }) {
+    const conditionalWidth = `${left87 ? 'w-[87%] ' : (left74 ? 'w-[74%]' : 'w-full ')}`;
+
+
     return (
-        <button disabled className={`${square ? 'w-11 ' : (left87 ? 'w-[87%] ' : 'w-full ')} h-11 flex justify-center items-center bg-slate-500 p-0 rounded-lg cursor-not-allowed focus:outline-none border-none transition-all`}>
+        <button disabled className={`${square ? 'w-11 ' : conditionalWidth} h-11 flex justify-center items-center bg-slate-500 p-0 rounded-lg cursor-not-allowed focus:outline-none border-none transition-all`}>
             <BtnLoader loader={loader} />
         </button>
     )
