43a831d5eed6a353b252fe2260afa6d61aad9d50
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-16 18:10:16 +0300

    Add time extraction method

--- client/src/utils/math.js
+++ client/src/utils/math.js
@@ -5,3 +5,18 @@ export const incrementBy1 = (v) => {
 export const decrementBy1 = (v) => {
     return Number(v) - 1 < 0 ? '0' : String(Number(v) - 1);
 };
+
+export const getTotalWorkoutTime = (w) => {
+    const [prep, cooldown, rest] = [w.preparation, w.cooldown, w.break];
+    const serviceTotal = Number(prep) + Number(cooldown);
+    const exercises = w.exercises;
+    const exerciseTotal = exercises.reduce((total, exercise) => {
+        return total + Number(exercise.duration) + Number(rest);
+    }, 0);
+    const sum = serviceTotal + exerciseTotal;
+    const min = Math.trunc(sum / 60);
+    const sec = sum % 60;
+
+    return `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`;
+
+};
