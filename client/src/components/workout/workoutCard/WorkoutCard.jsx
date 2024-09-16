f0373ad403ac01549c272518bae8c0e214c190f6
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-16 18:11:11 +0300

    Update workout card to display dynamic information. Update design and responsiveness.

--- client/src/components/workout/workoutCard/WorkoutCard.jsx
+++ client/src/components/workout/workoutCard/WorkoutCard.jsx
@@ -1,12 +1,24 @@
 import React, { useEffect } from 'react';
 import './WorkoutCard.css';
 import { FaPlay, FaEllipsisVertical, FaArrowLeft, FaGear, FaPlus } from "react-icons/fa6";
+import { getTotalWorkoutTime } from '../../../utils/math';
 
 
 function WorkoutCard({ color, i, handler, data = {} }) {
     const colorScheme = { "--bg-c": color };
     const workoutID = data?._id;
     const clickHandler = (e) => handler(e, workoutID);
+    const toPlot = [];
+    data?.exercises.slice(0, 2).forEach((x, i) => {
+        toPlot.push(x);
+        toPlot.push({ rest: data?.break });
+    });
+    const totalTime = getTotalWorkoutTime(data);
+    const intervalCount = 2 + (data?.exercises.length * 2);
+
+    useEffect(() => {
+        // console.log(data);
+    }, []);
 
     // Temp funciton
     const genActivities = (num) => {
@@ -22,22 +34,29 @@ function WorkoutCard({ color, i, handler, data = {} }) {
             style={colorScheme} onClick={clickHandler}>
             <section className="card__top text-white rounded-t-md w-full flex flex-nowrap flex-row items-center h-10">
                 <div className="h-full w-[calc(100%-5rem)] flex justify-start items-center pl-3 rounded-tl-md">
-                    <h3 className="text-lg font-bold text-ellipsis line-clamp-1">HIIT Circuit c{i}</h3>
+                    <h3 className="text-md font-bold text-left text-ellipsis line-clamp-1 tracking-wide">
+                        {data?.workoutName}
+                    </h3>
                 </div>
                 <div className="card__top-play card__top-btn h-full w-10 flex justify-center items-center text-xl hover:bg-slate-50/25 rounded-md hover:shadow-md active:scale-90"><FaPlay /></div>
                 <div className="card__top-menu card__top-btn h-full w-10 flex justify-center items-center text-xl hover:bg-slate-50/25 rounded-md hover:shadow-md active:scale-90"><FaEllipsisVertical /></div>
             </section>
-            <section className="card__mid flex flex-col pl-3 pt-1 justify-center items-start leading-5">
-                <p className="text-ellipsis line-clamp-1">1. Prepare: 30 sec</p>
-                <p className="text-ellipsis line-clamp-1">2. Work: 45 sec - {genActivities(1)}</p>
-                <p className="text-ellipsis line-clamp-1">3. Rest: 15 sec</p>
-                <p className="text-ellipsis line-clamp-1">4. Work: 45 sec - {genActivities(2)}</p>
-                <p className="text-ellipsis line-clamp-1">5. Rest: 15 sec</p>
+            <section className="card__mid flex flex-col pl-3 pt-1 justify-center items-start leading-5 font-semibold">
+                <p className="text-ellipsis line-clamp-1 text-[15px]">1. Prepare: {data?.preparation || 30}s</p>
+                {toPlot.map((x, i) => (
+                    <p key={`elist-${i}`} className="text-ellipsis line-clamp-1 text-[15px] mt-[1px]">
+                        {x?.rest ? (
+                            <>{i + 2}. Rest: {x?.rest}s</>
+                        ) : (
+                            <>{i + 2}. Work: {x?.duration || 60}s - {x?.exercise || genActivities(1)}</>
+                        )}
+                    </p>
+                ))}
                 <p>...</p>
             </section>
-            <section className="card__bottom flex flex-col pl-3 p-2 justify-center items-start rounded-b-md">
-                <p>Total Time: 08:35</p>
-                <p>Intervals: 26</p>
+            <section className="card__bottom flex flex-col pl-3 p-2 justify-center items-start rounded-b-md text-[15px] font-semibold">
+                <p>Total Time: <span className='tracking-wider'>{totalTime}</span> min</p>
+                <p>Intervals: {intervalCount}</p>
             </section>
         </article>
     )
